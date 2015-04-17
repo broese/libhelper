51d874c6a8a3bf476512d5a98a057dcd658fd9e7
Eduard Bröse <ed.broese@gmx.de>
2015-04-17 17:05:56 +0200

    lh_debug: rewrite LH_ERROR macro, provide LH_ERRORV variant for void functions, test functions for LH_ERROR

--- lh_debug.h
+++ lh_debug.h
@@ -21,51 +21,78 @@
 
 /*
 
-Define what method Libhelper should use when encountering errors.
-The method is defined by LH_ERR. The user either undefs LH_ERR,
-which results in default behavior, or defines it as one of the
-constants below.
+LH_ERROR(value, msg, arg...)
+    Produce an error message and bail out with given error code.
 
-Default method (#ifndef LH_ERR):
-LH returns defined error value for the function, e.g. a NULL for
-pointers or a negative value for integers. Does not print any
-errors. The user must analyze the return value and query the
-actual error through errno;
+LH_ERRORV(msg, arg)
+    Produce an error message and bail out with no specific code.
+    Use in void functions to avoid compiler warnings
 
-Message (LH_ERR_MESSAGE):
-LH prints an error message to STDERR and returns an error value
-to the caller.
+value  : error code/value to return (any type)
+msg    : printf format string with the message
+arg... : zero or more values to be included by the printf
 
-Abort (LH_ERR_ABORT):
-LH prints an error message to STDERR and then aborts the execution
-via exit(1).
+The exact behavior is determined by the value of LH_ERROR_BEHAVIOR:
+  LH_ERROR_QUIET   : do nothing
+  LH_ERROR_MESSAGE : only produce message
+  LH_ERROR_RETURN  : produce message on STDERR and return from function
+  LH_ERROR_EXIT    : produce message and abort program
+
+By default, the module will use LH_ERR_RETURN. If you want to change this
+behavior, set the appropriate value before including the module, for example:
+
+#define LH_ERROR_BEHAVIOR LH_ERROR_EXIT
+#include <lh_debug.h>
 
 */
 
-#define LH_ERR_MESSAGE 1
-#define LH_ERR_ABORT   2
+#define LH_ERROR_QUIET    0
+#define LH_ERROR_MESSAGE  1
+#define LH_ERROR_RETURN   2
+#define LH_ERROR_EXIT     3
 
-// TODO: come up with a concept how this should be handled for a compiled code
-#define LH_ERR LH_ERR_MESSAGE
+#ifndef LH_ERROR_BEHAVIOR
+#define LH_ERROR_BEHAVIOR LH_ERROR_RETURN
+#endif
 
-#ifndef LH_ERR
 
-#define LH_ERROR(retval,msg,...)                \
-    return retval;
+#if (LH_ERROR_BEHAVIOR==LH_ERROR_QUIET)
 
-#elif (LH_ERR==LH_ERR_MESSAGE)
+#define LH_ERROR(retval,msg,...) do { } while(0);
+#define LH_ERRORV(msg,...) LH_ERROR(0, msg, ##__VA_ARGS__)
 
-#define LH_ERROR(retval,msg,...) {                              \
-        fprintf(stderr,msg " : %s\n",##__VA_ARGS__,strerror(errno));  \
-        return retval;                                          \
+#elif (LH_ERROR_BEHAVIOR==LH_ERROR_MESSAGE)
+
+#define LH_ERROR(retval,msg,...) {                                      \
+        fprintf(stderr,msg " : %s\n",##__VA_ARGS__,strerror(errno));    \
     }
-    
-#elif (LH_ERR==LH_ERR_ABORT)
+#define LH_ERRORV(msg,...) LH_ERROR(0, msg, ##__VA_ARGS__)
 
-#define LH_ERROR(retval,msg,...) {                              \
-        fprintf(stderr,msg " : %s\n",##__VA_ARGS__,strerror(errno));  \
-        exit(1);                                                \
+#elif (LH_ERROR_BEHAVIOR==LH_ERROR_RETURN)
+
+#define LH_ERROR(retval,msg,...) {                                      \
+        fprintf(stderr,msg " : %s\n",##__VA_ARGS__,strerror(errno));    \
+        return retval;                                                  \
     }
+#define LH_ERRORV(msg,...) {                                            \
+        fprintf(stderr,msg " : %s\n",##__VA_ARGS__,strerror(errno));    \
+        return;                                                         \
+    }
+
+#elif (LH_ERROR_BEHAVIOR==LH_ERROR_EXIT)
+
+#define LH_ERROR(retval,msg,...) {                                      \
+        fprintf(stderr,msg " : %s\n",##__VA_ARGS__,strerror(errno));    \
+        exit((int)retval);                                              \
+    }
+#define LH_ERRORV(msg,...) {                                            \
+        fprintf(stderr,msg " : %s\n",##__VA_ARGS__,strerror(errno));    \
+        exit(1);                                                        \
+    }
+
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
 
 #endif
 
--- test/test_debug.c
+++ test/test_debug.c
@@ -1,10 +1,35 @@
-#include <lh_debug.h>
+/*
+ Authors:
+ Copyright 2012-2015 by Eduard Broese <ed.broese@gmx.de>
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version
+ 2 of the License, or (at your option) any later version.
+
+ lh_debug : functions for debug and error messages
+*/
+
 #include "lhtest.h"
 
 TF(warn, "warnings") {
 } _TF
 
+static int error_function(int val) {
+    errno=val;
+    LH_ERROR(val+1, "OK, val=%d", val);
+}
+
+static void void_error_function(int val) {
+    errno=val;
+    LH_ERRORV("OK, val=%d", val);
+}
+
 TF(error, "errors") {
+    fail += (error_function(1)!=2);
+    fail += (error_function(2)!=3);
+    fail += (error_function(3)!=4);
+    void_error_function(10);
 } _TF
 
 TF(here, "LH_HERE tracing") {
